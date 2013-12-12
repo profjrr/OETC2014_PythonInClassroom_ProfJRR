4b5899e3457978a662ccdc9da2cce1ed4d65b0b9
James Reed <cisprofjrr@alumni.ohio.edu>
2013-12-11 20:22:56 -0500

    Learning Module 1
    Python Basics:
    Installing
    Running
    Testing

---
+++ LearnModule01/demo01jan2014.py
@@ -0,0 +1,26 @@
+##Welcome to Python Programming for Your Classroom -- OETC2014 -- Prof. Reed
+##Supports Learning  Module 1 -- Python Basics -- installing, running & testing!
+##
+## Things to do:
+## Run1: "plain vanilla"
+## Run2: Pass a parameter
+## Run3: Using a test program to simply computer program testing!
+##
+
+## Professor Reed sez: "lazy evaluation and data typing"
+year = "2014"
+##year = 2014
+##
+
+def ourWelcome(year):
+##def ourWelcome():       ##alt1 "plain vanilla"
+    print("Welcome to the OETC Conference 2014")
+##    print("Welcome to the OETC Conference",year)
+    print("on Python Programming in the Classroom")
+    print("Here we go again!!!")
+
+def testOurWelcome():
+    ourWelcome(2014)
+    
+## end of demos by Professor Reed, cisprofjrr@alumni.ohio.edu
+##
